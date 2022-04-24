bceb12f089c36288a1c0e62227c3f479622136d8
りん <31785428+rin-senpai@users.noreply.github.com>
2022-04-24 15:38:25 +0800

    yeah

--- koharu.py
+++ koharu.py
@@ -2,20 +2,19 @@ import logging
 logging.basicConfig(level=logging.ERROR)
 
 from discord import Intents
-from cogs.utils.bot import Bot#, Help
+from bot import Bot
 
-from cogs.utils import setup
+from utils import setup
 config = setup.config()
 
 intents = Intents.all()
 
 bot = Bot(
-    command_prefix=config["prefix"],
-    prefix = config["prefix"],
-    owner_ids=config["owners"],
-    # help_command=Help(),
-    description=config["description"],
+    command_prefix=config['prefix'],
+    prefix = config['prefix'],
+    owner_id=config['owner'],
+    description=config['description'],
     intents=intents
-    )
+)
 
-bot.run(config["token"])
+bot.run(config['token'])
