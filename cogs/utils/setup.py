7f04beb73a9f28941572781caf918e6f7626a3e2
りん <31785428+rin-senpai@users.noreply.github.com>
2022-04-24 15:38:10 +0800

    delet

--- cogs/utils/setup.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def config(filename: str = "config"):
-    try:
-        with open(f"{filename}.json", encoding='utf8') as data:
-            return json.load(data)
-    except FileNotFoundError:
-        raise FileNotFoundError("JSON file wasn't found")
